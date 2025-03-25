d9cf096106cb0689c848f83bad7604082a960f1d
TuoNomeUtente <mighty_sham>
2025-03-25 15:04:16 +0100

    added the CRUD functionalities with some styling

--- src/app/task/components/confirmation-dialog/confirmation-dialog.component.ts
+++ src/app/task/components/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
 @Component({
-  selector: 'app-confirmation-dialog',
-  imports: [],
-  templateUrl: './confirmation-dialog.component.html',
+  selector: 'app-confirmation',
+  imports: [MatButtonModule, MatDialogActions, MatDialogTitle, MatDialogContent],
+  templateUrl: '../confirmation-dialog/confirmation-dialog.component.html',
   styleUrl: './confirmation-dialog.component.scss'
 })
 export class ConfirmationDialogComponent {
 
+  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>) { }
+
+  closeDialog(message: string) {
+    this.dialogRef.close(message);
+  }
 }
